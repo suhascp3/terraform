881b92ea4f18c10715aec8154668664bcd349cf3
Suhas <suhascrtr@gmail.com>
2022-08-12 19:50:39 +0530

    adding local variables

--- main.tf
+++ main.tf
@@ -1,23 +1,20 @@
-resource "aws_instance" "my_instance" {
-	ami = "ami-068257025f72f470d"
-	instance_type = "t2.micro"
-	count = 2 
+locals {
+  my_ami = "ami-068257025f72f470d"
+  instance_type = "t2.micro"
+} 
+
+resource "aws_instance" "my_instance1" {
+	ami = local.my_ami
+	instance_type = local.instance_type
 	tags = {
-	     Name = "terraform-instence"
+	     Name = "terraform-instence1"
 	}
 }
 
-resource "aws_key_pair" "TF_key" {
-  key_name   = "TF_key"
-  public_key = tls_private_key.rsa.public_key_openssh
-}
-
-resource "tls_private_key" "rsa" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "local_file" "TF_key" {
-    content  = tls_private_key.rsa.private_key_pem
-    filename = "tfkey"
+resource "aws_instance" "my_instance2" {
+	ami = local.my_ami
+	instance_type = local.instance_type
+	tags = {
+	     Name = "terraform-instence2"
+	}
 }
---
+++ versions.tf
@@ -0,0 +1,10 @@
+terraform {
+  
+      
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source = "hashicorp/aws"
+    }
+  }
+}   
